1fc480ce5b2061dd442b8cd41db5f6558c835f46
Zoe Mannion <10380186@mydbs.ie>
2019-01-10 18:54:30 +0000

    Add my first calculator with no tests.

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+def add(first, second):
+	return first + second
+
+def subtract(first, second):
+	return first - second
+
+print(add(2,2))
+
+print(subtract(2,2))
